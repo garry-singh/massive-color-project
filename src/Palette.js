7f7c9b6dec053023580be65ccc0d1f1051e52bee
Garry Singh <garry.singh@live.com.au>
2019-08-12 21:44:21 +1000

    Added seedPalettes and Palette

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+	render() {
+		return (
+			<div className="Palette">
+				{/* Navbar */}
+				<div className="Palette-colors">{/* Color Boxes */}</div>
+				{/* Footer */}
+			</div>
+		);
+	}
+}
+
+export default Palette;
